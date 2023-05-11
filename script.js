87cdd101a50b2576c1bde49363437a0ba8211059
Thomas Weichhart <multihacker007yt@gmail.com>
2023-05-11 15:21:23 +0200

    temporary

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+let alert_type_images = {
+    'p': '<td> <img src="https://image.jimcdn.com/app/cms/image/transf/dimension=100x10000:format=png/path/s47c677f812c00ba1/image/i0b7521ebe95b9430/version/1470063035/image.png" alt="" /> </td>',
+    's': '<td> <img src="https://image.jimcdn.com/app/cms/image/transf/dimension=100x10000:format=png/path/s47c677f812c00ba1/image/i5946598134795bc9/version/1470069114/image.png" alt="" /> </td>'
+};
+
+let type_images = {
+    'b': '<img src="https://image.jimcdn.com/app/cms/image/transf/dimension=120x10000:format=png/path/s47c677f812c00ba1/image/i41ccc95d7b5bfbe6/version/1531755152/image.png" width="30" height="30" alt="" />',
+    't': '<img src="https://image.jimcdn.com/app/cms/image/transf/dimension=320x10000:format=png/path/s47c677f812c00ba1/image/i26273cd4d48cb25f/version/1531755148/image.png" width="30" height="30" alt="" />',
+    's': '<img src="https://image.jimcdn.com/app/cms/image/transf/dimension=278x10000:format=gif/path/s47c677f812c00ba1/image/i398e35b7bba28a00/version/1531755156/image.gif" width="30" height="30" alt="" />'
+};
+
+let vehicle_images = {
+    'tlf': '<td> <a href="http://www.ff-boeheimkirchen.at/%C3%BCber-uns/fuhrpark/tlf-a/"><img src= "https://image.jimcdn.com/app/cms/image/transf/dimension=118x10000:format=png/path/s47c677f812c00ba1/image/i65e49bdd863c0ab8/version/1446024823/image.png" alt="" /></a> </td>',
+    'rf-s': '<td> <a href="http://www.ff-boeheimkirchen.at/%C3%BCber-uns/fuhrpark/rf-s/"><img src= "https://image.jimcdn.com/app/cms/image/transf/dimension=125x10000:format=png/path/s47c677f812c00ba1/image/i02b8c065ba22cf22/version/1446024826/image.png" alt="" /></a> </td>',
+    'mtf': '<td> <a href="https://www.ff-boeheimkirchen.at/%C3%BCber-uns/fuhrpark/mtf/"><img src= "https://image.jimcdn.com/app/cms/image/transf/dimension=110x10000:format=jpg/path/s47c677f812c00ba1/image/i2a56691932c87b5c/version/1502553907/image.jpg" alt="" /></a> </td>',
+    'vf': '<td> <a href="http://www.ff-boeheimkirchen.at/%C3%BCber-uns/fuhrpark/vf/"><img src= "https://image.jimcdn.com/app/cms/image/transf/dimension=105x10000:format=png/path/s47c677f812c00ba1/image/i2ad687b46b32d5a8/version/1446024814/image.png" alt="" /></a> </td>',
+    'klf': '<td> <a href="http://www.ff-boeheimkirchen.at/%C3%BCber-uns/fuhrpark/klf/"><img src= "https://image.jimcdn.com/app/cms/image/transf/dimension=95x10000:format=png/path/s47c677f812c00ba1/image/ia8690be94128764c/version/1446024810/image.png" alt="" /></a> </td>',
+    'wlf': '<td> <a href="http://www.ff-boeheimkirchen.at/%C3%BCber-uns/fuhrpark/wlf/"><img src= "https://image.jimcdn.com/app/cms/image/transf/dimension=118x10000:format=png/path/s47c677f812c00ba1/image/ic63cc508067c7c9b/version/1683354764/image.png" alt="" /></a> </td>'
+};
+
+let additional_crew = {
+    "Feuerwehren": {
+        "ausserkasten": {
+            "name": "FF Ausserkasten",
+            "link": "https://www.ff-ausserkasten.at/"
+        },
+        "mechters": {
+            "name": "FF Mechters",
+        },
+    },
+    "BOS": {
+        "rettung": {
+            "name": "Rettung",
+        },
+        "notarzt": {
+            "name": "Notarzt",
+        },
+        "polizei": {
+            "name": "Polizei",
+            "link": "https://www.polizei.gv.at/noe/start.aspx"
+        },
+    },
+    "Andere": {
+        "presse": {
+            "name": "NÖN",
+            "link": "https://www.noen.at/"
+        },
+
+    }
+};
+
 document.querySelector('#main-form').addEventListener('submit', function (e) {
     e.preventDefault();
 
@@ -20,29 +71,11 @@ document.querySelector('#main-form').addEventListener('submit', function (e) {
         vehicles.push(checkbox.value);
     });
 
-    let other_crew = document.querySelector('#other-crew').value;
     let report = document.querySelector('#report').value;
 
-    // presets
-    let alert_type_images = {
-        'p': '<td> <img src="https://image.jimcdn.com/app/cms/image/transf/dimension=100x10000:format=png/path/s47c677f812c00ba1/image/i0b7521ebe95b9430/version/1470063035/image.png" alt="" /> </td>',
-        's': '<td> <img src="https://image.jimcdn.com/app/cms/image/transf/dimension=100x10000:format=png/path/s47c677f812c00ba1/image/i5946598134795bc9/version/1470069114/image.png" alt="" /> </td>'
-    };
+    let other_crew_presets = document.querySelectorAll('div>select');
+    let other_crew = document.querySelector('#other-crew').value;
 
-    let type_images = {
-        'b': '<img src="https://image.jimcdn.com/app/cms/image/transf/dimension=120x10000:format=png/path/s47c677f812c00ba1/image/i41ccc95d7b5bfbe6/version/1531755152/image.png" width="30" height="30" alt="" />',
-        't': '<img src="https://image.jimcdn.com/app/cms/image/transf/dimension=320x10000:format=png/path/s47c677f812c00ba1/image/i26273cd4d48cb25f/version/1531755148/image.png" width="30" height="30" alt="" />',
-        's': '<img src="https://image.jimcdn.com/app/cms/image/transf/dimension=278x10000:format=gif/path/s47c677f812c00ba1/image/i398e35b7bba28a00/version/1531755156/image.gif" width="30" height="30" alt="" />'
-    };
-
-    let vehicle_images = {
-        'tlf': '<td> <a href="http://www.ff-boeheimkirchen.at/%C3%BCber-uns/fuhrpark/tlf-a/"><img src= "https://image.jimcdn.com/app/cms/image/transf/dimension=118x10000:format=png/path/s47c677f812c00ba1/image/i65e49bdd863c0ab8/version/1446024823/image.png" alt="" /></a> </td>',
-        'rf-s': '<td> <a href="http://www.ff-boeheimkirchen.at/%C3%BCber-uns/fuhrpark/rf-s/"><img src= "https://image.jimcdn.com/app/cms/image/transf/dimension=125x10000:format=png/path/s47c677f812c00ba1/image/i02b8c065ba22cf22/version/1446024826/image.png" alt="" /></a> </td>',
-        'mtf': '<td> <a href="https://www.ff-boeheimkirchen.at/%C3%BCber-uns/fuhrpark/mtf/"><img src= "https://image.jimcdn.com/app/cms/image/transf/dimension=110x10000:format=jpg/path/s47c677f812c00ba1/image/i2a56691932c87b5c/version/1502553907/image.jpg" alt="" /></a> </td>',
-        'vf': '<td> <a href="http://www.ff-boeheimkirchen.at/%C3%BCber-uns/fuhrpark/vf/"><img src= "https://image.jimcdn.com/app/cms/image/transf/dimension=105x10000:format=png/path/s47c677f812c00ba1/image/i2ad687b46b32d5a8/version/1446024814/image.png" alt="" /></a> </td>',
-        'klf': '<td> <a href="http://www.ff-boeheimkirchen.at/%C3%BCber-uns/fuhrpark/klf/"><img src= "https://image.jimcdn.com/app/cms/image/transf/dimension=95x10000:format=png/path/s47c677f812c00ba1/image/ia8690be94128764c/version/1446024810/image.png" alt="" /></a> </td>',
-        'wlf': '<td> <a href="http://www.ff-boeheimkirchen.at/%C3%BCber-uns/fuhrpark/wlf/"><img src= "https://image.jimcdn.com/app/cms/image/transf/dimension=118x10000:format=png/path/s47c677f812c00ba1/image/ic63cc508067c7c9b/version/1683354764/image.png" alt="" /></a> </td>'
-    };
 
     //get date in this form: ddmmyyyy
     let date_short = `${date.getDate().toString().padStart(2, '0')}${(date.getMonth() + 1).toString().padStart(2, '0')}${date.getFullYear()}`;
@@ -167,16 +200,34 @@ document.querySelector('#main-form').addEventListener('submit', function (e) {
                             <font size="+1">FF Böheimkirchen - Markt (${vehicles.length} Fahrzeug${vehicles.length == 1 ? '' : 'e'} + ${total_crew} Mann ‑ inkl. Bereitschaft)</font>
                         </li>
                         ${other_crew.split('\n').map((other_crew_member) => { return `<li><font size="+1">${other_crew_member}</font></li>`; }).join("")}
-                    </ul>
-                </td>
+                        ${other_crew_presets.forEach((preset) => {
+            let val = preset.value;
+            if (val == "") { return; }
+
+            for (types in additional_crew) {
+                for (participant in additional_crew[types]) {
+                    if (participant == val) {
+
+                        if (additional_crew[types][val].hasOwnProperty('link')) {
+                            return `<li><font size="+1"><a href="${additional_crew[types][val].link}">${additional_crew[types][val].name}</a></font></li>`;
+                        } else {
+                            return `<li><font size="+1">${additional_crew[types][val].name}</font></li>`;
+                        }
+                    }
+                }
+            }
+
+        })}
+                    </ul >
+                </td >
                 <td>
                 </td>
-            </tr>
-        </table>
-    </div>`;
+            </tr >
+        </table >
+    </div > `;
     } else {
         // generate the homepage snippet
-        report_html = `<tr border="0" cellpadding="0">
+        report_html = `< tr border = "0" cellpadding = "0" >
     <td valign="middle" align="left">
         ${type_images[type]}
     </td>
@@ -189,7 +240,7 @@ document.querySelector('#main-form').addEventListener('submit', function (e) {
     <td valign="middle">
         ${location}
     </td>
-</tr>`;
+</tr > `;
     }
 
     let output_element = document.querySelector('#output')
@@ -241,11 +292,41 @@ document.querySelectorAll('.multi-space>span').forEach(span => {
 
 // only show the level 4 when type is Brandeinsatz
 document.querySelectorAll('input[name="type"]').forEach(input => {
-    input.addEventListener('change', function (e) {
+    input.addEventListener('change', e => {
         if (e.target.value == "b") {
             document.querySelector('#level-4-span').style.display = "flex";
         } else {
             document.querySelector('#level-4-span').style.display = "none";
         }
     });
+});
+
+let select_element = document.createElement('select');
+select_element.name = "other-crew-selectable";
+select_element.innerHTML = `< option value = "" ></option > `;
+for (let group in additional_crew) {
+    let optgroup = document.createElement('optgroup');
+    optgroup.label = group;
+    for (let crew in additional_crew[group]) {
+        let option = document.createElement('option');
+        option.value = crew;
+        option.innerText = additional_crew[group][crew].name;
+        optgroup.appendChild(option);
+    }
+    select_element.appendChild(optgroup);
+}
+
+let main_selector = document.querySelector('#other-crew-list');
+
+main_selector.appendChild(select_element);
+
+main_selector.addEventListener('change', e => {
+    if (e.target.value == "") {
+        if (document.querySelectorAll('#other-crew-list>select').length > 1) {
+            e.target.remove();
+        }
+        return;
+    }
+    let new_other_crew_list = main_selector.firstElementChild.cloneNode(true);
+    main_selector.appendChild(new_other_crew_list);
 });
